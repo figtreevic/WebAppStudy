72755b5ff71c00ccd764f4fc3f3690520fef295d
figtreevic <figtreevic@gmail.com>
2022-05-31 17:58:38 -0300

    Study upload

---
+++ class1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class MySecondClass
+{
+    public static void Main()
+    {
+        int res;
+        res= 5 + 3;
+
+
+        Console.WriteLine("The value of res is {0}.", res);
+        Console.ReadLine();
+    }
+}
